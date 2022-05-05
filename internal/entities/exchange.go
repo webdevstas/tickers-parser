3d963c87c1fdf51c70f885eee39cb7a3724eeb1c
webdevstas <spirin.stas@mail.ru>
2022-05-05 23:11:29 +0300

    implemented fetchTickers incapsulation

--- internal/entities/exchange.go
+++ internal/entities/exchange.go
@@ -1,8 +1,10 @@
 package entities
 
 import (
-	"gorm.io/gorm"
+	"fmt"
 	"tickers-parser/internal/types"
+
+	"gorm.io/gorm"
 )
 
 type IExchange interface {
@@ -11,15 +13,21 @@ type IExchange interface {
 
 type Exchange struct {
 	gorm.Model
-	Key        string `json:"key" db:"key"`
-	Name       string `json:"name,omitempty" db:"name"`
-	Enabled    bool   `json:"enabled,omitempty" db:"enabled"`
-	TickersUrl string `json:"tickersUrl" db:"tickersUrl"`
-	IExchange  `gorm:"-"`
+	Key       string `json:"key" db:"key"`
+	Name      string `json:"name,omitempty" db:"name"`
+	Enabled   bool   `json:"enabled,omitempty" db:"enabled"`
+	IExchange `gorm:"-"`
 }
 
 type ExchangeTickers struct {
 	Exchange  string
 	Timestamp int64
 	Tickers   []types.ExchangeRawTicker
+}
+
+func (e *Exchange) FetchTickers() ([]types.ExchangeRawTicker, error) {
+	if e.IExchange == nil {
+		return nil, fmt.Errorf("not implemented fetch tickers method for exchange %v", e.Key)
+	}
+	return e.IExchange.FetchTickers()
 }
